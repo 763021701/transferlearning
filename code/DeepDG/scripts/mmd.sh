635d57eb87dcca3e26053f02c172e3358fe02a02
huangpu <763021701@qq.com>
2025-04-01 06:08:24 +0000

    update scripts
    Signed-off-by: huangpu <763021701@qq.com>

--- code/DeepDG/scripts/mmd.sh
+++ code/DeepDG/scripts/mmd.sh
@@ -1,31 +1,32 @@
 dataset='PACS'
 algorithm=('MLDG' 'ERM' 'DANN' 'RSC' 'Mixup' 'MMD' 'CORAL' 'VREx' 'CFD')
 algorithm2=('MLDG2' 'ERM2' 'DANN2' 'RSC2' 'Mixup2' 'MMD2' 'CORAL2' 'VREx2' 'CFD2')
-test_envs=0
-gpu_id=0
+test_envs=1
+gpu_id=3
 n_workers=8
 data_dir='~/workspace/datasets/Homework3-PACS/PACS/'
 max_epoch=120
 batch_size=32
-net='resnet18'
+net='vgg16'
+net_pt_weight='default'
 task='img_dg'
 
 # MMD
 #i=5
 #mmd_gamma=0.5
 #lr=0.005
-#output="/home/ubuntu/workspace/project/transferlearning/code/DeepDG/output/${algorithm[$i]}-$net-$lr-$mmd_gamma-$dataset$test_envs"
+#output="/home/ubuntu/workspace/project/transferlearning/code/DeepDG/output/${algorithm[$i]}[$net_pt_weight]-$net-$lr-$mmd_gamma-$dataset$test_envs"
 
 #python train.py --data_dir $data_dir --max_epoch $max_epoch --net $net --task $task --output $output \
 #       --test_envs $test_envs --dataset $dataset --algorithm ${algorithm[i]} --lr $lr --mmd_gamma $mmd_gamma \
-#       --batch_size $batch_size --N_WORKERS $n_workers --gpu_id $gpu_id
+#       --batch_size $batch_size --N_WORKERS $n_workers --gpu_id $gpu_id --net_pt_weight $net_pt_weight
 
 # MMD2
 i=5
-mmd_gamma=0.5
+mmd_gamma=1
 lr=0.005
-output="/home/ubuntu/workspace/project/transferlearning/code/DeepDG/output/${algorithm[$i]}-$net-$lr-$mmd_gamma-$dataset$test_envs"
+output="/home/ubuntu/workspace/project/transferlearning/code/DeepDG/output/${algorithm2[$i]}[$net_pt_weight]-$net-$lr-$mmd_gamma-$dataset$test_envs"
 
 python train.py --data_dir $data_dir --max_epoch $max_epoch --net $net --task $task --output $output \
        --test_envs $test_envs --dataset $dataset --algorithm ${algorithm2[i]} --lr $lr --mmd_gamma $mmd_gamma \
-       --batch_size $batch_size --N_WORKERS $n_workers --gpu_id $gpu_id
+       --batch_size $batch_size --N_WORKERS $n_workers --gpu_id $gpu_id --net_pt_weight $net_pt_weight
--- code/DeepDG/scripts/myrun.sh
+++ code/DeepDG/scripts/myrun.sh
@@ -1,10 +1,13 @@
 dataset='PACS'
 algorithm=('MLDG' 'ERM' 'DANN' 'RSC' 'Mixup' 'MMD' 'CORAL' 'VREx' 'CFD')
-test_envs=3
-gpu_ids=0
+test_envs=2
+gpu_id=0
+n_workers=8
 data_dir='~/workspace/datasets/Homework3-PACS/PACS/'
 max_epoch=120
+batch_size=32
 net='resnet18'
+net_pt_weight='default'
 task='img_dg'
 
 # MLDG
@@ -24,10 +27,11 @@ task='img_dg'
 i=8
 cfd_gamma=1
 lr=0.005
-cfd_alpha=0.5
-cfd_beta=0.5
-output="/home/ubuntu/workspace/project/transferlearning/code/DeepDG/output/${algorithm[$i]}-$net-$lr-$cfd_gamma-$cfd_alpha-$cfd_beta-$dataset$test_envs"
+cfd_alpha=0.8
+cfd_beta=0.2
+output="/home/ubuntu/workspace/project/transferlearning/code/DeepDG/output/${algorithm[$i]}[$net_pt_weight]-$net-$lr-$cfd_gamma-$cfd_alpha-$cfd_beta-$dataset$test_envs"
 
 python train.py --data_dir $data_dir --max_epoch $max_epoch --net $net --task $task --output $output \
---test_envs $test_envs --dataset $dataset --algorithm ${algorithm[i]} --lr $lr --cfd_gamma $cfd_gamma \
---cfd_alpha $cfd_alpha --cfd_beta $cfd_beta --N_WORKERS 8
+                --test_envs $test_envs --dataset $dataset --algorithm ${algorithm[i]} --lr $lr \
+                --cfd_gamma $cfd_gamma --cfd_alpha $cfd_alpha --cfd_beta $cfd_beta --batch_size $batch_size \
+                --N_WORKERS $n_workers --gpu_id $gpu_id --net_pt_weight $net_pt_weight
