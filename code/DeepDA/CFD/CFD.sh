27f5aac04dd4307c007a2264647eb202617366bb
huangpu <763021701@qq.com>
2025-04-23 07:49:29 +0000

    update script
    Signed-off-by: huangpu <763021701@qq.com>

--- code/DeepDA/CFD/CFD.sh
+++ code/DeepDA/CFD/CFD.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-GPU_ID=1
 data_dir=/home/ubuntu/workspace/datasets/OFFICE31
 output_dir=/home/ubuntu/workspace/project/transferlearning/code/DeepDA/output/CFD
 
@@ -8,15 +7,14 @@ if [ ! -d "$output_dir" ]; then
     mkdir -p "$output_dir"
 fi
 
-# Office31
-CUDA_VISIBLE_DEVICES=$GPU_ID python main.py --config CFD/CFD.yaml --data_dir $data_dir --src_domain dslr --tgt_domain amazon | tee "$output_dir/CFD_D2A.log"
-CUDA_VISIBLE_DEVICES=$GPU_ID python main.py --config CFD/CFD.yaml --data_dir $data_dir --src_domain dslr --tgt_domain webcam | tee "$output_dir/CFD_D2W.log"
-
-CUDA_VISIBLE_DEVICES=$GPU_ID python main.py --config CFD/CFD.yaml --data_dir $data_dir --src_domain amazon --tgt_domain dslr | tee "$output_dir/CFD_A2D.log"
-CUDA_VISIBLE_DEVICES=$GPU_ID python main.py --config CFD/CFD.yaml --data_dir $data_dir --src_domain amazon --tgt_domain webcam | tee "$output_dir/CFD_A2W.log"
-
-CUDA_VISIBLE_DEVICES=$GPU_ID python main.py --config CFD/CFD.yaml --data_dir $data_dir --src_domain webcam --tgt_domain amazon | tee "$output_dir/CFD_W2A.log"
-CUDA_VISIBLE_DEVICES=$GPU_ID python main.py --config CFD/CFD.yaml --data_dir $data_dir --src_domain webcam --tgt_domain dslr | tee "$output_dir/CFD_W2D.log"
+# 使用GNU Parallel并行执行任务
+parallel -j 6 ::: \
+"CUDA_VISIBLE_DEVICES=1 python main.py --config CFD/CFD.yaml --data_dir $data_dir --src_domain dslr --tgt_domain amazon | tee \"$output_dir/CFD_D2A.log\"" \
+"CUDA_VISIBLE_DEVICES=2 python main.py --config CFD/CFD.yaml --data_dir $data_dir --src_domain dslr --tgt_domain webcam | tee \"$output_dir/CFD_D2W.log\"" \
+"CUDA_VISIBLE_DEVICES=3 python main.py --config CFD/CFD.yaml --data_dir $data_dir --src_domain amazon --tgt_domain dslr | tee \"$output_dir/CFD_A2D.log\"" \
+"CUDA_VISIBLE_DEVICES=1 python main.py --config CFD/CFD.yaml --data_dir $data_dir --src_domain amazon --tgt_domain webcam | tee \"$output_dir/CFD_A2W.log\"" \
+"CUDA_VISIBLE_DEVICES=2 python main.py --config CFD/CFD.yaml --data_dir $data_dir --src_domain webcam --tgt_domain amazon | tee \"$output_dir/CFD_W2A.log\"" \
+"CUDA_VISIBLE_DEVICES=3 python main.py --config CFD/CFD.yaml --data_dir $data_dir --src_domain webcam --tgt_domain dslr | tee \"$output_dir/CFD_W2D.log\""
 
 
 
